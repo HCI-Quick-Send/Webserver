3189431657715b08b5cfdcfade396da882eee4a3
vatsalvora <vatsal1@msn.com>
2015-02-26 12:57:38 -0500

    Fixing imports

--- server.js
+++ server.js
@@ -5,8 +5,6 @@
 var express	= require('express');
 var cors 	= require('cors');
 var app		= express();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
 var bodyParser	= require('body-parser');
 
 var dbConfig = require('./app/config/db');
