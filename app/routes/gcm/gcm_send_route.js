e2bb445fe40d45b504e8c3184651d5cfe3e34a45
vatsalvora <vatsal1@msn.com>
2015-03-31 00:36:15 -0400

    Message

--- app/routes/gcm/gcm_send_route.js
+++ app/routes/gcm/gcm_send_route.js
@@ -5,7 +5,7 @@ module.exports = function(router, GCMDB, knex)
 	router.route('/gcm/send')
 	.post(function(req,res) {
 		var arr = req.body.users.split(',');
-		var data = {users:arr, url: req.body.url, sender:req.body.sender};
+		var data = {users:arr, url: req.body.url, message:req.body.message,sender:req.body.sender};
 		gcm(data,[],GCMDB, knex);
 		res.send({success:true});
 	});
